9c1f88b89acfcd86a09881a5df452ddda39296e1
SouL-H <mhmtaydinw@gmail.com>
2022-03-09 21:55:15 +0300

    ipfs change and 3.rd party package add

--- src/config.js
+++ src/config.js
@@ -5,9 +5,9 @@ const { NETWORK } = require(`${basePath}/constants/network.js`);
 const network = NETWORK.eth;
 
 // General metadata for Ethereum
-const namePrefix = "Your Collection";
-const description = "Remember to replace this description";
-const baseUri = "ipfs://NewUriToReplace";
+const namePrefix = "SouLH";
+const description = "TestSouLH NFT";
+const baseUri = "ipfs://QmXm334z9g9XjNyN5NJB4T9Kxa8oGVboTKRcRs78piLvG1";
 
 const solanaMetadata = {
   symbol: "YC",
